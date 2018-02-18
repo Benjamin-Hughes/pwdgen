62aeef72b34049f5d1902712e4a0b9f81ea70251
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2018-02-18 22:22:07 +0000

    add app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = { passwords: [] };
+
+  componentDidMount() {
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    fetch('/api/passwords')
+    .then(res => res.json())
+    .then(passwords => this.setState({ passwords }))
+    .catch(error => console.log(error.message))
+    console.log(this.state.passwords.length)
+  }
+
   render() {
-    return (
+    const {passwords}  = this.state;
+
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {passwords.length ? (
+        <div>
+          <h1>5 passwords</h1>
+          <ul className="passwords">
+            {passwords.map((password, index) => {
+              <li key={index}>
+                {password}
+              </li>
+            })}
+          </ul>
+          <button
+            className="more"
+            onClick={this.getPasswords}>
+            Get more
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1> No Passwords :(</h1>
+          <button
+            className="more"
+            onClick={this.getPasswords}>
+            Try again?
+          </button>
+        </div>
+      )}
       </div>
     );
   }
