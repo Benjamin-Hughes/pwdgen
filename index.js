d37ebb20bf336e407268e98acd5f11687fe09136
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2018-02-18 17:58:37 +0000

    fix typo in catch all route

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/api/passwords', (req, res) => {
 	console.log(`Sent ${count} passwords`);
 });
 
-app.get('*' (req, res) => {
+app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
